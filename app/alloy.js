823bda0a9df0ca56d6f93319d65c2ba90eeb4e80
francoisvl <francois@vanlille.name>
2016-09-21 15:03:26 +1000

    Add shout place icon
    + correct mate input field icons position
    + correct mate input screen’s scrollview content height
    + add device singleton in alloy.js for cross-platform access to device
    info

--- app/alloy.js
+++ app/alloy.js
@@ -15,6 +15,7 @@
 // wrap your code in a self-executing function.
 (function (global) {
 	// We pass `this` so you could use `global.foo` to force a global variable.
+    "use strict";
 
     global.Toast = require("toast");
     global.Log = require("logger");
@@ -41,11 +42,58 @@
      * Called from index controller once intro animation is complete
      */
     Alloy.Globals.initNavigation = function() {
-        "use strict";
         // Require in the navigation module
         Alloy.Globals.Navigator = require("navigation")({
             parent : Alloy.Globals.navigationWindow || null
         });
+    };
+
+    ///////////////////////////////////////////////////////////////////////////////
+    //
+    // Device singleton
+    //
+    ///////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * Device information, some come from the Ti API calls and can be referenced
+     * from here so multiple bridge calls aren't necessary, others generated here
+     * for ease of calculations and such.
+     *
+     * @type {Object}
+     * @param {String} version The version of the OS
+     * @param {Number} versionMajor The major version of the OS
+     * @param {Number} versionMinor The minor version of the OS
+     * @param {Number} width The width of the device screen
+     * @param {Number} height The height of the device screen
+     * @param {Number} dpi The DPI of the device screen
+     * @param {String} orientation The device orientation, either "landscape" or "portrait"
+     * @param {String} statusBarOrientation A Ti.UI orientation value
+     */
+    Alloy.Globals.Device = {
+        version : Ti.Platform.version,
+        versionMajor : parseInt(Ti.Platform.version.split(".")[0], 10),
+        versionMinor : parseInt(Ti.Platform.version.split(".")[1], 10),
+        width : (Ti.Platform.displayCaps.platformWidth > Ti.Platform.displayCaps.platformHeight) ? Ti.Platform.displayCaps.platformHeight : Ti.Platform.displayCaps.platformWidth,
+        height : (Ti.Platform.displayCaps.platformWidth > Ti.Platform.displayCaps.platformHeight) ? Ti.Platform.displayCaps.platformWidth : Ti.Platform.displayCaps.platformHeight,
+        dpi : Ti.Platform.displayCaps.dpi,
+        orientation : ((Ti.Gesture.orientation == Ti.UI.LANDSCAPE_LEFT || Ti.Gesture.orientation == Ti.UI.LANDSCAPE_RIGHT) ? "landscape" : "portrait")
+    };
+
+    if (OS_IOS) {
+        Alloy.Globals.Device.heightExcludingNav = Alloy.Globals.Device.height - 70;
+    }
+    if (OS_ANDROID) {
+        Alloy.Globals.Device.heightExcludingNav = Alloy.Globals.Device.height - 80;
+        Alloy.Globals.Device.width = (Alloy.Globals.Device.width / (Alloy.Globals.Device.dpi / 160));
+        Alloy.Globals.Device.height = (Alloy.Globals.Device.height / (Alloy.Globals.Device.dpi / 160));
+    }
+
+    Alloy.Globals.dpToPx = function(dp) {
+        return dp * (Ti.Platform.displayCaps.platformHeight / Alloy.Globals.Device.height);
+    };
+
+    Alloy.Globals.pxToDp = function(px) {
+        return px * (Alloy.Globals.Device.height / Ti.Platform.displayCaps.platformHeight);
     };
 
 })(this);
